e5b927aaa7af1babd34dcc796bea5f7bee4aa7bf
John Mettraux <jmettraux@gmail.com>
2014-12-10 10:03:45 +0900

    scaffold site spec

---
+++ spec/site_spec.c
@@ -0,0 +1,19 @@
+
+//
+// specifying aranye
+//
+// Wed Dec 10 10:01:08 JST 2014
+//
+
+#include "fara_dom.h"
+#include "fara_haml.h"
+
+
+context "site:"
+{
+  describe "x()"
+  {
+    it "flips burgers"
+  }
+}
+
